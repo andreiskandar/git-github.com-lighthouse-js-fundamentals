2128d3b0e05f02f29e9a10ab10a56258f1e6ce7a
aisk-coding <56459037+aisk-coding@users.noreply.github.com>
2020-07-02 10:08:53 -0700

    learning array and distructuring an array

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Add a flavour
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Getting first flavour
+console.log(iceCreamFlavours[0]);
+
+//Getting the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//Getting total number of flavours
+console.log(iceCreamFlavours.length);
